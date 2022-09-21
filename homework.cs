07b6212fac90c7adff666c7083f64d0106840d35
fakhri mehdi <95130267+minato1133@users.noreply.github.com>
2022-09-21 21:55:31 +0200

    Add files via upload
    c# loops and arrays

---
+++ homework.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homework : MonoBehaviour
+{
+    
+              
+    // Start is called before the first frame update
+     void Start()
+        {
+           
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    public void DestroyObjects(string tag) { 
+                GameObject[] gameObjects = GameObject.FindGameObjectsWithTag(tag);
+            foreach (GameObject target in gameObjects)
+            {
+                GameObject.Destroy(target);
+            }
+}
+    }
